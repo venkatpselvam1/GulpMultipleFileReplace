1eeb63b9fc6d2011a9a7ad8456876121c047687e
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-03-28 19:14:02 +0530

    adding copy command to copy files from src to output folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    console.log("test");
-    cb();
-  }
-  
-  exports.default = defaultTask
+const { src, dest } = require('gulp');
+let keysToReplace = 
+{
+  "grape" : "Muscat",
+  "mango" : "Malgoa Mango",
+  "banana" : "Red Banana"
+};
+
+function replace_files_by_dict(cb){
+  return src('src/*.json')
+  .pipe(dest('output/'));
+  cb();
+}
+
+exports.default = replace_files_by_dict
