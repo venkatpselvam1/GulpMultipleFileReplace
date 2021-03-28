2bc224b8b4e775b6d8a21aabd3ca695b8a071d5c
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-03-28 19:22:16 +0530

    demo for fails to replace multiple content

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,22 @@ let keysToReplace =
   "banana" : "Red Banana"
 };
 
-function replace_files_by_dict(cb){
+function replace_files_by_singe_word(cb){
   return src('src/*.json')
   .pipe(replace("grape", "Muscat"))
   .pipe(dest('output/'));
+}
+
+// This will fail to replace multiple content.
+function replace_files_by_dict_with_for(cb){
+  for(var key in keysToReplace){
+    console.log("Trying to replace " + key +"by " + keysToReplace[key]);
+    src('src/*.json')
+  .pipe(replace(key, keysToReplace[key]))
+  .pipe(dest('output/'));
+  }
   cb();
 }
 
-exports.default = replace_files_by_dict
+// exports.default = replace_files_by_singe_word
+exports.default = replace_files_by_dict_with_for
