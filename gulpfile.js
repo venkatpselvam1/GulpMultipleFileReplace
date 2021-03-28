62755ff40f73b6092b921a210388bb1c0854749f
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-03-28 20:21:32 +0530

    using rxjs and replacing demo text

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,62 @@
-var replace = require('gulp-string-replace');
-const { src, dest } = require('gulp');
+var gulp_replace = require('gulp-string-replace');
+var replace = require('replace');
+const { src, dest, series } = require('gulp');
+const { from } =require('rxjs');
+
+// typically this is an API response.
 let keysToReplace = 
 {
-  "grape" : "Muscat",
-  "mango" : "Malgoa Mango",
-  "banana" : "Red Banana"
+  "brown" : "black",
+  "quick" : "slow",
+  "lazy" : "busy",
+  "dog" : "donkey",
+  "cat" : "mouse"
 };
 
-function replace_files_by_singe_word(cb){
-  return src('src/*.json')
-  .pipe(replace("grape", "Muscat"))
+// this will work but the problem is we need to hard code the keys.
+function multiple_replace_using_hardcoded_pipe(cb){
+  return src('src/*.txt')
+  .pipe(gulp_replace("brown", "black"))
+  .pipe(gulp_replace("lazy", "busy"))
+  .pipe(gulp_replace("dog", "donkey"))
   .pipe(dest('output/'));
 }
 
+// Here we replaced the hard coded pipe with for
 // This will fail to replace multiple content.
-function replace_files_by_dict_with_for(cb){
+function multiple_replace_using_gulp_replace_fails(cb){
   for(var key in keysToReplace){
     console.log("Trying to replace " + key +"by " + keysToReplace[key]);
-    src('src/*.json')
-  .pipe(replace(key, keysToReplace[key]))
-  .pipe(dest('output/'));
+    src('src/*.txt')
+      .pipe(gulp_replace(key, keysToReplace[key]))
+      .pipe(dest('output/'));
   }
   cb();
 }
 
-// exports.default = replace_files_by_singe_word
-exports.default = replace_files_by_dict_with_for
+//copy file to the new location
+function copyFiles(){
+  return src('src/*.txt')
+  .pipe(dest('output/'));
+}
+
+//replace using rxjs
+function multiple_replace_using_rxjs_subscribe_passes(cb){
+  var keys = Object.keys(keysToReplace);
+  from(keys).subscribe(key => 
+    {
+      console.log(key+" " + keysToReplace[key]);
+      replace({
+        regex: key,
+        replacement: keysToReplace[key],
+        paths: ['output/'],
+        recursive: true,
+        silent: true,
+      });
+    });
+  cb();
+}
+
+exports.hardcoded_pipe = multiple_replace_using_hardcoded_pipe
+exports.failure_for_with_pip = multiple_replace_using_gulp_replace_fails
+exports.default = series(copyFiles, multiple_replace_using_rxjs_subscribe_passes)
